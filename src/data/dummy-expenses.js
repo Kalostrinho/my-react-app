4a468423052452319c0097bb4c38e456435cddf2
AlanZinho <jfkalostro@gmail.com>
2023-04-04 10:30:54 -0600

    Added dummy data for expenses

---
+++ src/data/dummy-expenses.js
@@ -0,0 +1,26 @@
+export const expenses = [
+  {
+    id: "e1",
+    description: "Toilet Paper",
+    price: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    description: "New TV",
+    price: 799.49,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: "e3",
+    description: "Car Insurance",
+    price: 294.67,
+    date: new Date(2022, 2, 28),
+  },
+  {
+    id: "e4",
+    description: "New Desk (Wooden)",
+    price: 450,
+    date: new Date(2022, 5, 12),
+  },
+];
