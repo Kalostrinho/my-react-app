13d07c03ed6fc424e0a70f96503ec763d913be43
AlanZinho <jfkalostro@gmail.com>
2023-04-04 10:31:46 -0600

    Adjusted overall app with new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { ExpenseItem } from './components/expense-item/ExpenseItem'
+import { expenses } from './data/dummy-expenses'
+
 function App() {
   return (
-    <div className="App">
+    <div>
       <header>
-        <p>
+        <h1>
           Let's <code>code</code> the fuck on!
-        </p>
+        </h1>
       </header>
+      <ExpenseItem 
+        description={expenses[0].description}
+        price={expenses[0].price}
+        date={expenses[0].date}
+      />
+      <ExpenseItem 
+        description={expenses[1].description}
+        price={expenses[1].price}
+        date={expenses[1].date}
+      />
+      <ExpenseItem 
+        description={expenses[2].description}
+        price={expenses[2].price}
+        date={expenses[2].date}
+      />
+      <ExpenseItem 
+        description={expenses[3].description}
+        price={expenses[3].price}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
